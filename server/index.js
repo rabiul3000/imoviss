fc26f21618865aa36746d4cd260fb2666f6ee5b0
rabiul3000 <mdrabiul.asia@gmail.com>
2024-08-03 17:06:46 +0600

    for deployment

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ import { connectDB } from './config/database.js';
 import { app, server } from './socket/socket.js';
 import cspConfig from './utils/cspConfig.js';
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -22,11 +22,11 @@ app.use(morgan('dev'));
 app.get('/', (req,res) => res.status(200).json('server is running'))
 app.use('/api', routes);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+// app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
+// app.get('*', (req, res) => {
+// 	res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+// });
 
 const port = process.env.PORT;
 
