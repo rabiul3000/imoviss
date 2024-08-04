1e9a21e93b18d5e55b5af1f11b3e05979bbfca08
rabiul3000 <mdrabiul.asia@gmail.com>
2024-08-04 14:16:18 +0600

    fixing some code

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import { connectDB } from './config/database.js';
 import { app, server } from './socket/socket.js';
 import cspConfig from './utils/cspConfig.js';
 
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -18,10 +19,13 @@ app.use(helmet({ contentSecurityPolicy: cspConfig }));
 app.use(cookieParser());
 app.use(morgan('dev'));
 
-
-app.get('/', (req, res) => res.status(200).json('server is running'))
 app.use('/api', routes);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 const port = process.env.PORT;
 
